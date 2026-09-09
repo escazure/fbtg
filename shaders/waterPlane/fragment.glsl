2bfac570c4da2ce94fc7671fc94e5635d6be3978
escazure <haiper.1453@gmail.com>
2026-09-09 23:04:05 +0200

    Added water warping

--- shaders/waterPlane/fragment.glsl
+++ shaders/waterPlane/fragment.glsl
@@ -18,18 +18,26 @@ const vec3 shallowWater = vec3(10.0, 77.0, 104.0) / 255.0;
 const vec3 waterFoam = vec3(232.0, 249.0, 253.0) / 255.0;
 
 vec2 hash22(vec2 p){
-    p = vec2(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5, 183.3)));
-    return fract(sin(p) * 43758.5453123);
+	uvec2 v = floatBitsToUint(p);
+	v.x += 1664525u;
+	v ^= v.yx * 1664525u;
+	v *= 1664525u;
+	v ^= v >> 16u;
+	v ^ v.yx >> 5u;
+
+	uvec2 floatBits = (v & 0x007FFFFFu) | 0x3F800000u;
+	vec2 res = uintBitsToFloat(floatBits) - 1.0;
+	return res;
 }
 
-float perlinNoise(vec2 uv){
+vec3 perlinNoiseDerivatives(vec2 uv){
 	vec2 i = floor(uv);
 	vec2 f = fract(uv);
 
-	vec2 gradA = hash22(i) * 2.0 - 1.0;
-	vec2 gradB = hash22(i + vec2(1.0, 0.0)) * 2.0 - 1.0;
-	vec2 gradC = hash22(i + vec2(0.0, 1.0)) * 2.0 - 1.0;
-	vec2 gradD = hash22(i + vec2(1.0, 1.0)) * 2.0 - 1.0;
+	vec2 gradA = normalize(hash22(i) * 2.0 - 1.0);
+	vec2 gradB = normalize(hash22(i + vec2(1.0, 0.0)) * 2.0 - 1.0);
+	vec2 gradC = normalize(hash22(i + vec2(0.0, 1.0)) * 2.0 - 1.0);
+	vec2 gradD = normalize(hash22(i + vec2(1.0, 1.0)) * 2.0 - 1.0);
 
 	float dotA = dot(gradA, f);
 	float dotB = dot(gradB, f - vec2(1.0, 0.0));
@@ -37,11 +45,21 @@ float perlinNoise(vec2 uv){
 	float dotD = dot(gradD, f - vec2(1.0, 1.0));
 
 	vec2 w = f * f * f * (f * (f * 6.0 - 15.0) + 10.0);
+	vec2 dw = f * f * (f * (f * 30.0 - 60.0) + 30.0);
 
 	float u1 = mix(dotA, dotB, w.x);
 	float u2 = mix(dotC, dotD, w.x);
+	float val = mix(u1, u2, w.y);
 
-	return mix(u1, u2, w.y);
+	vec2 g1 = mix(gradA, gradB, w.x);
+	vec2 g2 = mix(gradC, gradD, w.x);
+	vec2 gradInterpolation = mix(g1, g2, w.y);
+
+	float dx = mix(dotB - dotA, dotD - dotC, w.y) * dw.x;
+	float dy = mix(dotC - dotA, dotD - dotB, w.x) * dw.y;
+	vec2 d = gradInterpolation + vec2(dx, dy);
+	
+	return vec3(val, d);
 }
 
 float voronoiNoise(vec2 uv, float cellSize){
@@ -78,8 +96,8 @@ float waterNoise(vec2 uv, int octaves, float baseDistortion, float baseFrequency
 	float e = baseExponent;
 	for(int i = 0; i < octaves; i++){
 		vec2 suv = uv * f;
-	    float px = perlinNoise(suv) * d;
-		float py = perlinNoise(suv + offset) * d;
+	    float px = perlinNoiseDerivatives(suv).x * d;
+		float py = perlinNoiseDerivatives(suv + offset).x * d;
 	    float v = voronoiNoise(suv + vec2(px, py), 1.0);
 	    res += pow(v, e);
 	    f *= lacunarity;
@@ -122,13 +140,25 @@ vec3 getWaterColor(){
 
 void main(){
 	vec3 I = normalize(worldPos - uCameraPos);
+
+	vec2 noiseUV = worldPos.xz * 0.15;
+	vec2 waveSlope = perlinNoiseDerivatives(noiseUV).yz;
+
+	vec2 uvDeriv = fwidth(noiseUV);
+    float pixelFootprint = max(uvDeriv.x, uvDeriv.y);
+    float noiseFilter = 1.0 - smoothstep(0.6, 0.8, pixelFootprint);
+    waveSlope *= noiseFilter;
+
 	vec3 N = vec3(0.0, 1.0, 0.0);
 	vec3 R = reflect(I, N);
 	vec3 skyColor = getSkyColor(R);
 
 	vec2 ndc = (clipSpacePos.xy / clipSpacePos.w) * 0.5 + 0.5;
-	vec2 reflectUV = vec2(ndc.x, ndc.y);
-	vec4 terrainReflection = texture(uReflectionMap, reflectUV);
+	vec2 reflectUV = ndc;
+	vec2 offsetStrength = vec2(0.002);
+
+	vec2 warpedUV = clamp(reflectUV + (waveSlope * offsetStrength), 0.001, 0.999);
+	vec4 terrainReflection = texture(uReflectionMap, warpedUV);
 
 	vec3 waterColor = getWaterColor();
 
