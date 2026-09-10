a7ebe503a4d2eb845ea843e8adfaa9272149431d
escazure <139878904+escazure@users.noreply.github.com>
2026-09-10 17:09:46 +0200

    Delete build directory

--- build/CMakeFiles/copy_notices.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_notices"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_notices.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
